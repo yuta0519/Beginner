a2c11db5c1f58ba75fd493f3ecc474b83a61e7ac
唐松 祐太 <karamatsuyuuta@karamatsu-no-iMac.local>
2014-04-09 23:27:36 +0900

    Initial Commit

---
+++ 演習2-2-1/演習2-2-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  演習2-2-1
+//
+//  Created by 唐松 祐太 on 2014/04/09.
+//  Copyright (c) 2014年 唐松 祐太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
