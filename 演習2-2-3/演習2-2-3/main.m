01b9f08345303747cfd606c7f9cd40aa15d081e3
唐松 祐太 <karamatsuyuuta@karamatsu-no-iMac.local>
2014-04-10 23:18:08 +0900

    update1

---
+++ 演習2-2-3/演習2-2-3/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  演習2-2-3
+//
+//  Created by 唐松 祐太 on 2014/04/10.
+//  Copyright (c) 2014年 唐松 祐太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"こんにちは");
+        NSLog(@"objective-c");
+        
+        
+    }
+    return 0;
+}
+
