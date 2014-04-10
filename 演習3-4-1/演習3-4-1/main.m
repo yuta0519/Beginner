51f1d20919ff37bfa873ad0a76d0bc18a4913b91
唐松 祐太 <karamatsuyuuta@karamatsu-no-iMac.local>
2014-04-10 23:53:27 +0900

    update2

---
+++ 演習3-4-1/演習3-4-1/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  演習3-4-1
+//
+//  Created by 唐松 祐太 on 2014/04/10.
+//  Copyright (c) 2014年 唐松 祐太. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        int a; //変数の宣言
+        a = 5; //値の代入
+        NSLog(@"変数aの値は%dです\n",a);//格納されている値の表示
+        
+    }
+    return 0;
+}
+
