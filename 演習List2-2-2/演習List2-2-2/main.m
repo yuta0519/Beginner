a0f74a2a05b6824bf62ce4434d6697b0f39d3b3c
唐松 祐太 <karamatsuyuuta@karamatsu-no-iMac.local>
2014-04-10 23:25:27 +0900

    update1

--- 演習List2-2-2/演習List2-2-2/main.m
+++ 演習List2-2-2/演習List2-2-2/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  演習2-2-1
+//  演習List2-2-2
 //
-//  Created by 唐松 祐太 on 2014/04/09.
+//  Created by 唐松 祐太 on 2014/04/10.
 //  Copyright (c) 2014年 唐松 祐太. All rights reserved.
 //
 
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"こんにちは、objective-c");
+        NSLog(@"こんにちは\nobjective-c");
         
     }
     return 0;
